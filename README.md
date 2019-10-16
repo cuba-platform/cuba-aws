42def646a52896ad18cfcd74dadcec2213db8874
Artem Chizhikov <chizhikov@haulmont.com>
2019-10-16 15:50:36 +0300

    Amazon Web Services Addon initial commit (2397)

---
+++ README.md
@@ -0,0 +1,52 @@
+[![license](https://img.shields.io/badge/license-Apache%20License%202.0-blue.svg?style=flat)](http://www.apache.org/licenses/LICENSE-2.0)
+
+# Overview
+
+CUBA Amazon Add-on enables using supported AWS services, for now it supports:
+* Amazon Simple Storage Service (S3)
+
+# Installation
+
+1. Open your application in CUBA Studio.
+
+2. Double-click *Add-ons*.
+
+3. Click the *Gear* button and select *Install by coordinates* item.
+
+    ![Adding custom component1](img/adding_component1.png)
+
+4. Specify the coordinates of the component in the corresponding field as follows: group:name:version.
+
+   ![Adding component2](img/adding_component2.png)
+
+   * Artifact group: *com.haulmont.addon.cubaaws*
+   * Artifact name: *cubaaws-global*
+   * Version: *add-on version*
+
+# Amazon S3 File Storage configuration
+
+ Add the following line into `spring.xml`:
+ ```xml
+     <bean name="cuba_FileStorage" class="com.haulmont.addon.cubaaws.s3.AmazonS3FileStorage"/>
+ ```
+
+ Add the following properties into `app.properties`:
+
+ * **cuba.amazonS3.accessKey**: Amazon S3 access key
+
+ * **cuba.amazonS3.secretAccessKey**: Amazon S3 secret access key
+
+ * **cuba.amazonS3.region**: Amazon S3 region
+
+ * **cuba.amazonS3.bucket**: Amazon S3 bucket name
+
+ * **cuba.amazonS3.chunkSize** (optional): Amazon S3 chunk size
+
+ **Example:**
+ ```ini
+ cuba.amazonS3.accessKey = AAAABBBBCCCCDD11CC22
+ cuba.amazonS3.secretAccessKey = AbCD+eFFGK3iAB9Ca9BCAB7ddDDABcabCabc9aBC
+ cuba.amazonS3.region = eu-north-1
+ cuba.amazonS3.bucket = s3-ec2-test
+ cuba.amazonS3.chunkSize = 5500
+ ```
---
+++ modules/core/src/com/haulmont/addon/cubaaws/s3/AmazonS3Config.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2008-2019 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.haulmont.addon.cubaaws.s3;
+
+import com.haulmont.cuba.core.config.Config;
+import com.haulmont.cuba.core.config.Property;
+import com.haulmont.cuba.core.config.Source;
+import com.haulmont.cuba.core.config.SourceType;
+import com.haulmont.cuba.core.config.defaults.DefaultInt;
+import com.haulmont.cuba.core.global.Secret;
+
+/**
+ * Configuration parameters interface used by cubaaws add-on.
+ */
+@Source(type = SourceType.APP)
+public interface AmazonS3Config extends Config {
+
+    /**
+     * @return Amazon S3 access key.
+     */
+    @Property("cuba.amazonS3.accessKey")
+    String getAccessKey();
+
+    /**
+     * @return Amazon S3 secret access key.
+     */
+    @Secret
+    @Property("cuba.amazonS3.secretAccessKey")
+    String getSecretAccessKey();
+
+    /**
+     * @return Amazon S3 region.
+     */
+    @Property("cuba.amazonS3.region")
+    String getRegionName();
+
+    /**
+     * @return Amazon S3 bucket name.
+     */
+    @Property("cuba.amazonS3.bucket")
+    String getBucket();
+
+    /**
+     * @return Amazon S3 chunk size.
+     */
+    @Property("cuba.amazonS3.chunkSize")
+    @DefaultInt(8192)
+    int getChunkSize();
+}
